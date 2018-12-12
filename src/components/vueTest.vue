7e8128ff7424ff4d12fb40c7ae2d997321de0093
fuhaoliang <fuhaoliang@juran.com.cn>
2018-12-12 21:14:38 +0800

    vuetest

---
+++ src/components/vueTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>测试npm发布</h1>
+    <h2>props: (msg: {{ msg }})</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'VueTest',
+  props: {
+    msg: {
+      required: false,
+      default: 'noProp'
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
+
+import vueTest from '../src/components/vueTest'
+export default vueTest
